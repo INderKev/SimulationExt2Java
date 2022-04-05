1c8c6583c1718b1b35c51e258c98c759fa5b90f5
INderKev <66580607+INderKev@users.noreply.github.com>
2022-04-05 13:05:33 -0500

    Sistema de archivos

--- simulation/src/logica/SistemaArchivos.java
+++ simulation/src/logica/SistemaArchivos.java
@@ -1,14 +1,10 @@
 package logica;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import logica.Bloque;
-import logica.Inodo;
+
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+
 
 public class SistemaArchivos {
     //cantidad de Inodos y bloques que se pueden crear
@@ -19,41 +15,76 @@ public class SistemaArchivos {
 
     public SistemaArchivos(){
         this.bitmapInodos = new int [100];
-	    this.bitmapBloques = new int [100];
+	this.bitmapBloques = new int [100];
         this.inodos = new ArrayList<Inodo>();
     }
     
     //public void crearInodo(String nombre, int idPadre, String permisos){
     public int crearInodo(int tipo, String nombre, int idPadre){ 
-        
-	int idNodo =asignarBitmapInodos(), idBloque=asignarBitmapBloques();
+        //variables que contendrán el di de los bloque e inodos según su posición en el array
+	int idInodoActual =asignarBitmapInodos(), idBloque=asignarBitmapBloques(), idBloqueAux = -1;
         int indexPadre = 0;
-        //Busca el bloque del padre para ponerle que tiene un hijo
-        if(idPadre != 0){
+        BloqueCarpetas bloque;
+        //Busca el Inodo del padre para ponerle que tiene un hijo
+        if(this.inodos.size()==0){
+            indexPadre = 0;
+        }else{
             indexPadre = buscarPadre(idPadre);
         }
-        
+   
         //Revisa si ocurrio un error al asignar un identificador, no hay más espacio en los mapas de bits
-        if(idNodo == -1 || idBloque == -1 || indexPadre == -1){
-            System.out.println("No hay más espacio en los mapas de bits o no se encontro el id del padre");
+        if(idInodoActual == -1 || idBloque == -1 ||indexPadre ==-1 ){
+            System.out.println("No hay más espacio en los mapas de bits o no se encontro el padre");
             return -1;
-        }       
-        // se crea el bloque que contendra el id del padre y el actual
-        Bloque bloque = new Bloque(idPadre, idBloque);
-        // se añade el inodo a la lista de inodos
-	inodos.add(new Inodo(nombre ,idNodo, 1, obtenerFechaYHoraActual(), bloque ));
+        }           
+  
         // se pone en el bloque del padre el id del hijo
-        this.inodos.get(indexPadre).getApuntadorDirecto().setIdHijo(idNodo);
-       
-        return 0;
+        if( this.inodos.size()!=0){
+            
+            if(this.inodos.get(indexPadre).setIdHijo(idInodoActual, nombre)==-1){
+                //es necesario otro apuntador 
+                idBloqueAux = asignarBitmapBloques();
+                //el apuntador directo está ocupado por lo cual se utilizará un apuntador indirecto
+                int resultado = this.inodos.get(indexPadre).setHIjoIndirecto(idBloque, idBloqueAux, idInodoActual, nombre);
+                switch(resultado){
+                    case -1:
+                        // no hay espacio en el apuntador indirecto, se devuelven los ids
+                        devolverIdBloque(idBloqueAux);
+                        devolverIdBloque(idBloque);
+                        return -1;
+                    case 0:
+                        //se utilizaron todos los id de bloques por lo cual es necesario pedir otro id
+                        bloque = new BloqueCarpetas(idBloque, asignarBitmapBloques(), indexPadre);
+                        // se añade el inodo a la lista de inodos
+                        inodos.add(new Inodo(idInodoActual, tipo, bloque));
+                        return 0;
+                    case 1:
+                        //solo se utilizo un apuntador  idBloque, queda libre idBloqueAux
+                        bloque = new BloqueCarpetas(idBloque, idBloqueAux, indexPadre);
+                        // se añade el inodo a la lista de inodos
+                        inodos.add(new Inodo(idInodoActual, tipo, bloque));
+                        return 0;               
+                }
+            }else{
+                System.out.println("entro");
+                //se agrego al apuntador directo
+                bloque = new BloqueCarpetas(idBloque, idInodoActual, idPadre);
+                // se añade el inodo a la lista de inodos
+                inodos.add(new Inodo(idInodoActual, tipo, bloque));
+                return 0; 
+            }
+        }else{
+            //para el primer inodo
+            bloque = new BloqueCarpetas(idBloque, idInodoActual, idInodoActual);
+            inodos.add(new Inodo(idInodoActual, tipo, bloque));
+            return 0; 
+        }
+        //ocurrio un problema
+        System.out.println("no entro");
+        return -1;
     }
 
-    private String obtenerFechaYHoraActual(){
-        Date date = Calendar.getInstance().getTime();  
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
-        String strDate = dateFormat.format(date);
-        return strDate;
-    }
+    
     
     private int buscarPadre(int idPadre){
         for (int i = 0; i < this.inodos.size(); i++) {
@@ -76,14 +107,23 @@ public class SistemaArchivos {
         return -1;
     }
     
+    //pone en el mapa de bits de bloque 0 en los id que no se usaron
+    private void devolverIdBloque(int idBloque){
+        for(int i = 0; i<this.bitmapBloques.length; i++){
+		//revisa el primer espacio vacio de bitmaps
+		if(this.bitmapBloques[i]==idBloque){
+			this.bitmapBloques[i] = 0;
+		}
+	}
+    }
+    
     private int asignarBitmapInodos(){
         //asigna el valor de ocupado en el bitmap de inodos
     	for(int i = 0; i<this.bitmapInodos.length; i++){
 		//revisa el primer espacio vacio de bitmaps
 		if(this.bitmapInodos[i]==0){
 			this.bitmapInodos[i] = 1;
-			return i;
-			
+			return i;	
 		}
 	}
         return -1;
@@ -92,14 +132,20 @@ public class SistemaArchivos {
     public void imprimirDatos(){
         System.out.println("BLOQUES");
         for (int i = 0; i < this.bitmapBloques.length; i++) {
-            System.out.println(this.bitmapBloques[i]);
+            System.out.print(this.bitmapBloques[i]);
         }
-        System.out.println("INODOS");
+        System.out.println("\nINODOS");
         for (int i = 0; i < this.bitmapInodos.length; i++) {
-            System.out.println(this.bitmapInodos[i]);
+            System.out.print(this.bitmapInodos[i]);
         }
         for (int i = 0; i < this.inodos.size(); i++) {
-            System.out.println(this.inodos.get(i));
+            System.out.println("\nInodo id: "+this.inodos.get(i).getId());
+            HashMap<String,Integer> aux = this.inodos.get(i).getApuntadorDirecto().getCarpetaId();
+            System.out.println("Apuntador directo");
+            for (Iterator<String> it = aux.keySet().iterator(); it.hasNext();) {
+                String val = it.next();
+                System.out.println("llave: "+val + " valor: "+ aux.get(val));
+            }
         }
     }
  	
